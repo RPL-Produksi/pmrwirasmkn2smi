204cb4ea1dea2db80a698a2d62a9f4aa4b4373aa
Muhamad Hilal <hilal.muhammad0807@gmail.com>
2025-05-24 22:35:22 +0700

    feat: add feature fillament for post blog

--- app/Http/Controllers/BlogsController.php
+++ app/Http/Controllers/BlogsController.php
@@ -2,11 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Blog;
 use Illuminate\Http\Request;
 
 class BlogsController extends Controller
 {
-    public function index(){
-        return view('blogs.index');
+    public function index()
+    {
+        $data['blogs'] = Blog::where('status', 'published')
+            ->where('published_at', '<=', now())
+            ->orderBy('published_at', 'desc')
+            ->paginate(10);
+
+        return view('blogs.index')->with($data);
+    }
+
+    public function detail(Request $request, $slug)
+    {
+        $data['blog'] = Blog::where('slug', $slug)
+            ->where('status', 'published')
+            ->where('published_at', '<=', now())
+            ->firstOrFail();
+
+        return view('blogs.detail')->with($data);
     }
 }
